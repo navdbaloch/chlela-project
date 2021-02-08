35a14432a4cd42081196c5a89f04b951abeeccb8
Naveed Baloch <naveed.baloch@keeptruckin.com>
2021-02-08 15:40:36 +0500

    - configured backend with typescript

---
+++ backend/src/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+const app = express();
+const port = 8080; // default port to listen
+
+// define a route handler for the default home page
+app.get("/", (req, res) => {
+    res.send("Hello world!");
+});
+
+// start the Express server
+app.listen(port, () => {
+});
